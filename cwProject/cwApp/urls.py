569be47696c52773eddf9efd2338d8d6c6667f59
autumn-ragland <raglanda998@gmail.com>
2019-02-26 12:16:47 -0600

    cw submit

---
+++ cwProject/cwApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('page2', views.two, name='page2'),
+    path('page3', views.three, name='page3'),
+    path('page4', views.four, name='page4'),
+    path('page5', views.five, name='page5'),
+    path('drinks', views.cocktails, name='drinks'),
+    path('details/<int:drinkID>', views.details, name='details'),
+]
---
+++ cwProject/cwApp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Cocktail
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    context = {
+        'name': 'home'
+    }
+    return render(request, 'cwApp/home.html', context)
+
+
+def two(request):
+    context = {
+        'name': ' from home'
+    }
+    return render(request, 'cwApp/Page2.html', context)
+
+def three(request):
+    context = {
+        'name': ' from page2'
+    }
+    return render(request, 'cwApp/Page3.html', context)
+
+def four(request):
+    context = {
+        'name': ' from page3'
+    }
+    return render(request, 'cwApp/Page4.html', context)
+
+def five(request):
+    context = {
+        'name': ' from page4'
+    }
+    return render(request, 'cwApp/Page5.html', context)
+
+
+def cocktails(request):
+    allDrinks = Cocktail.objects.all()
+    context = {
+        'drinks': allDrinks
+    }
+    return render(request, 'cwApp/cocktail.html', context)
+
+
+def details(request, drinkID):
+    drinks = get_object_or_404(Cocktail, pk=drinkID)
+    context = {
+        'drink': drinks
+    }
+    return render(request, 'cwApp/details.html', context)
+
